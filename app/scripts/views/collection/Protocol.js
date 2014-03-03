fcd9b2d2667b73463b4ddaa2e69ef327f7449584
Keith Vedananda <kvedananda@gmail.com>
2014-03-02 22:58:06 -0500

    Fix view whitespace

--- app/scripts/views/collection/Protocol.js
+++ app/scripts/views/collection/Protocol.js
@@ -1,28 +1,28 @@
 define([
 	'backbone',
 	'views/item/Protocol'
-],
-function( Backbone, Protocol  ) {
-    'use strict';
+	],
+	function( Backbone, Protocol  ) {
+		'use strict';
 
-	/* Return a ItemView class definition */
-	return Backbone.Marionette.CollectionView.extend({
+		/* Return a ItemView class definition */
+		return Backbone.Marionette.CollectionView.extend({
 
-		initialize: function() {
-			console.log("initialize a Protocol CollectionView");
-		},
-		
-    	itemView: Protocol,
-    	
+			initialize: function() {
+				console.log("initialize a Protocol CollectionView");
+			},
+			
+			itemView: Protocol,
+			
 
-    	/* ui selector cache */
-    	ui: {},
+			/* ui selector cache */
+			ui: {},
 
-		/* Ui events hash */
-		events: {},
+			/* Ui events hash */
+			events: {},
 
-		/* on render callback */
-		onRender: function() {}
+			/* on render callback */
+			onRender: function() {}
+		});
+
 	});
-
-});
--- app/scripts/views/collection/Source.js
+++ app/scripts/views/collection/Source.js
@@ -1,28 +1,28 @@
 define([
 	'backbone',
 	'views/item/Source'
-],
-function( Backbone, Source  ) {
-    'use strict';
+	],
+	function( Backbone, Source  ) {
+		'use strict';
 
-	/* Return a ItemView class definition */
-	return Backbone.Marionette.CollectionView.extend({
+		/* Return a ItemView class definition */
+		return Backbone.Marionette.CollectionView.extend({
 
-		initialize: function() {
-			console.log("initialize a Source CollectionView");
-		},
-		
-    	itemView: Source,
-    	
+			initialize: function() {
+				console.log("initialize a Source CollectionView");
+			},
+			
+			itemView: Source,
+			
 
-    	/* ui selector cache */
-    	ui: {},
+			/* ui selector cache */
+			ui: {},
 
-		/* Ui events hash */
-		events: {},
+			/* Ui events hash */
+			events: {},
 
-		/* on render callback */
-		onRender: function() {}
+			/* on render callback */
+			onRender: function() {}
+		});
+
 	});
-
-});
