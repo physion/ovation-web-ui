7c77f33b9c41c0f4e38819d9c017a0b4c0fc7721
Keith Vedananda <kvedananda@gmail.com>
2014-05-26 17:35:01 -0400

    OvationWebController and DataViewController cleanup

--- app/scripts/controllers/DataView.js
+++ app/scripts/controllers/DataView.js
@@ -11,39 +11,40 @@ function( Backbone, DataViewLayout, ModelNavigatorController, EntityViewerContro
 	return Backbone.Marionette.Controller.extend({
 
 		initialize: function( options ) {
-			var dataViewLayout = new DataViewLayout({
-				model: options.model
-			});
+
+			var dataViewLayout = new DataViewLayout({}),
+				modelNavigatorController,
+				entityViewController,
+				metadataViewerController,
+				self = this,
+				lazyLayout;
+
 			options.region.show(dataViewLayout);
 			this.dataViewLayout = dataViewLayout;
 
-			var modelNavigatorController = new ModelNavigatorController({
-				model: options.model,
+			modelNavigatorController = new ModelNavigatorController({
 				region: dataViewLayout.modelNavigator
 			});
 
-			var entityViewController = new EntityViewerController({
-			});
+			entityViewController = new EntityViewerController({});
 
-			var metadataViewerController = new MetadataViewerController({
+			metadataViewerController = new MetadataViewerController({
 				region: dataViewLayout.metadataViewer
 			});
 
-			var self = this,
-				lazyLayout = _.debounce(function() {
-					self.resize();
-				}, 300);
+			lazyLayout = _.debounce(function() {
+				self.resize();
+			}, 300);
 			$(window).resize(function() {
 				self.resize();
 			});
-
 			this.resize();
 
 		},
 
 		resize: function() {
-			var availableWidth = this.dataViewLayout.$el.width();
-			var entityViewerWidth = availableWidth - $('#model-navigator-region').outerWidth();
+			var availableWidth = this.dataViewLayout.$el.width(),
+				entityViewerWidth = availableWidth - $('#model-navigator-region').outerWidth();
 			this.dataViewLayout.$el.find('#entity-viewer').outerWidth(entityViewerWidth);
 		}
 	});
